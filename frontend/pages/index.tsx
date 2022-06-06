b05b07b734f84dbd87d5a6cd10fa116519d8b2d2
sangyeop910 <fhakd6395@naver.com>
2022-06-06 18:47:18 +0900

    push fronted folder

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,114 +1,250 @@
 import type { NextPage } from 'next'
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { 
+import {
     Button,
+    ButtonGroup,
+    FormControl,
+    FormLabel,
+    HStack,
     Input,
     InputGroup,
     InputLeftAddon,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
     PinInput,
     PinInputField,
+    useDisclosure,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { currentUserIDState } from '../contexts/user';
+import { NodeNextRequest } from 'next/dist/server/base-http/node';
+import { now } from 'lodash';
+import style from '../styles/Home.module.css';
 
+const loginUrl = "http://127.0.0.1:8000/login";
+const authUrl = "http://127.0.0.1:8000/auth";
 
-const Home: NextPage = () => {
-    let [Name, setName] = useState("");
-    let [PhoneNumber, setPhoneNumber] = useState("");
-    let [Code, setCode] = useState("");
+export const Home: NextPage = () => {
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [code, setCode] = useState("");
+    const [isCode, setIsCode] = useState(false);
+    const [_, setUserId] = useRecoilState(currentUserIDState);
+    const router = useRouter()
+    const handleName: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        setName(event.target.value);
+    }
+    const [isPinErr, setIsPinErr] = useState(true);
+    const [isUserErr, setIsUserErr] = useState(true);
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const submitHandler = (event) => {
+    const Timer = () => {
+        const [min, setMin] = useState(3);
+        const [sec, setSec] = useState(0);
+        const time = useRef(20);
+        const timerld = useRef(null);
+
+        useEffect(() => {
+            timerld.current = setInterval(() => {
+                setMin(parseInt(time.current / 60));
+                setSec(time.current % 60);
+                time.current -= 1;
+            }, 1000);
+
+            return () => clearInterval(timerld.current);
+        }, []);
+
+        useEffect(() => {
+            if(time.current < 0){
+                console.log('타임아웃 시 발생 이벤트 작성')
+                clearInterval(timerld.current);
+            }
+        }, [sec]);
+
+        return(
+            <div className={style.timer}>{min} : {sec}</div>
+        )
+    }
+
+    const handlePhoneNumber: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        setPhoneNumber(event.target.value);
+    }
+
+    const handleCode: ((value: string) => void) = (value) => {
+        setCode(value)
+    }
+
+    const handleLogin: React.FormEventHandler<HTMLFormElement> = (event) => {
         event.preventDefault();
-        console.log(body);
+        const data = {
+            name: name,
+            phone_num: phoneNumber,
+        }
+
+        axios.post(loginUrl, data, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://127.0.0.1:3000"
+            }
+        }).then((res) => {
+            setIsCode(false)
+            console.log(res)
+        })
     }
 
-    function Click_LogIn(){
-      let userN = userName.value;
-      let userP = userPhone.value;
-      Name = userN;
-      PhoneNumber = userP;
-      let data = {
-        name: String(Name),
-        phone_num: String(PhoneNumber)
-      };
-      let link = "http://127.0.0.1:8000/login";
-      axios.post(link, data).then((res) => console.log(res))
-      return(
-          code.style.display = "block",
-          console.log(data),
-          setName(Name),
-          setPhoneNumber(PhoneNumber),
-      );
-  }
-
-    let body = {
-        Name : Name,
-        PhoneNumber : PhoneNumber
-    };
-
-    /*axios.post('/client', body).then(function (response) {
-        console.log("서버로 전송 성공")      
-    })
-    .catch(function (error) {
-        console.log("서버 전송 실패");
-    });
-
-    axios.get('/client').then(function (response) {
-        console.log("서버에서 받아오기 성공")      
-    })
-    .catch(function (error) {
-        console.log("서버에서 받아오기 실패");
-    });*/
-
-    const Click_Pin = () => {
-        let pinArray = [];
-        let pinNumber;
-        //window.location.assign('/main');
-        for(let i=0; i<6; i++){
-            pinArray.push(code.children[i].value);
-        }
-        pinNumber = pinArray.reduce((accumulator, currentNumber) => accumulator + currentNumber);
-        Code=pinNumber;
-        let data = {
-          name: Name,
-          phone_num: PhoneNumber
-        }
-        let Pin = Number(Code)
-        let link = "http://127.0.0.1:8000/auth/" + String(Pin);
-        axios.post(link, data).then((res) => console.log(res))
-        setCode(Code);
-        console.log(pinNumber);
-    }
-
-    const CodeSubmitHandler = (event) => {
+    const handleAuth: React.FormEventHandler<HTMLFormElement> = (event) => {
         event.preventDefault();
-        console.log(Code);
-    }
+        const data = {
+            name: name,
+            phone_num: phoneNumber,
+        }
+        
+        axios.post(authUrl+`/${code}`, data).then((res) => {
+            if (res.status == 200){
+                setUserId(res.data._id)
+                router.push('/user')
+            }
 
+        }).catch((e)=>{
+            let errorCode = e.response.status;
+            console.log(errorCode);
+            if (errorCode == 400){//400: 핀번호 오류 - 핀번호 재입력 유도
+                console.log(errorCode, "pin num error");
+                setIsPinErr(false)
+                onOpen()
+                setCode("");
+            }else if(errorCode == 408){//408: 시간초과 - 핀번호 백으로 다시 요청해서 새로 받기
+                console.log(errorCode, "time out");
+            }else if(errorCode == 404){//404: 사용자 데이터 입력 오류 - 사용자 정보(이름, 전화번호) 재입력
+                console.log(errorCode, "other reason err");
+                setIsUserErr(false)
+                onOpen()
+                setName("");
+                setPhoneNumber("");
+            }
+        })
+    }
+    
     return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <label>Name</label>
-                  <Input id='userName'></Input>
-                <label>PhoneNumber</label>
-                  <InputGroup>
-                  <InputLeftAddon children='+82' />
-                      <Input id='userPhone' type='tel' placeholder='phone number' />
-                  </InputGroup>
-                <Button  colorScheme='blue' onClick = {Click_LogIn}>Login</Button>
-            </form>
-            <form onSubmit = {CodeSubmitHandler} id = "code" style = {{display: "none"}}>
-                        <PinInput>
-                            <PinInputField />
-                            <PinInputField />
-                            <PinInputField />
-                            <PinInputField />
-                            <PinInputField />
-                            <PinInputField />
-                        </PinInput>
-                <Button colorScheme='blue' id = "btn" onClick = {Click_Pin}>확인</Button>
-            </form>
+        <div className={style.container}>
+            <div className={style.total}>
+                <div className={style.logInArea}>
+                    <form className={style.logInForm} onSubmit={isCode ? handleLogin : handleAuth}>
+                        <div className={style.headText}>
+                            <p>TAS<br></br>Log In</p>
+                        </div>
+                        <FormControl isRequired>
+                            <FormLabel htmlFor='name' style={{
+                                color: 'white'
+                            }}>Name</FormLabel>
+                            <Input
+                                id='name'
+                                value={name}
+                                onChange={handleName}
+                                placeholder='name'
+                                style={{color: 'white'}}
+                            />
+                        </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel htmlFor='phoneNumber' style={{
+                                color: 'white'
+                            }}>PhoneNumber</FormLabel>
+                            <InputGroup>
+                                <InputLeftAddon children='+82' />
+                                <Input
+                                    id='phoneNumber'
+                                    value={phoneNumber}
+                                    onChange={handlePhoneNumber}
+                                    type='tel'
+                                    placeholder='phone number'
+                                    style={{color: 'white'}}
+                                />
+                            </InputGroup>
+                        </FormControl>
+                        {
+                            isCode ? (
+                                    <button className={style.pinSubmit}
+                                        type='submit'
+                                    >
+                                        인증번호 받기
+                                    </button>
+                            ) : (
+                                    <div className={style.pinInput}>
+                                        <div>
+                                            <PinInput otp value={code} onChange={handleCode} style={{
+                                                borderColor : 'white',
+                                            }}>
+                                                <PinInputField style={{color: 'white'}}/>
+                                                <PinInputField style={{color: 'white'}}/>
+                                                <PinInputField style={{color: 'white'}}/>
+                                                <PinInputField style={{color: 'white'}}/>
+                                                <PinInputField style={{color: 'white'}}/>
+                                                <PinInputField style={{color: 'white'}}/>
+                                            </PinInput>
+                                        </div>
+                                        <button className={style.Submit} type='submit'>
+                                            확인
+                                        </button>
+                                        <Timer />
+                                    </div>
+                            )
+                        }                
+                    </form>
+                </div>
+            </div>
+            {
+                isPinErr ? (
+                    <div></div>
+                ) : (
+                    <div>
+                        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                            <ModalOverlay />
+                            <ModalContent>
+                                <ModalHeader>400 Error</ModalHeader>
+                            <ModalCloseButton />
+                                <ModalBody>
+                                    핀 번호를 다시 확인해주세요.
+                                </ModalBody>
+                            <ModalFooter>
+                                <Button onClick={onClose}>
+                                    확인
+                                </Button>
+                            </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </div>
+                )
+            }
+            {
+                isUserErr ? (
+                    <div></div>
+                ) : (
+                    <div>
+                        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                            <ModalOverlay />
+                            <ModalContent>
+                                <ModalHeader>404 ERROR</ModalHeader>
+                            <ModalCloseButton />
+                                <ModalBody>
+                                    올바르지 않은 유저 정보입니다.
+                                </ModalBody>
+                            <ModalFooter>
+                                <Button onClick={onClose}>
+                                    확인
+                                </Button>
+                            </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </div>
+                )
+            }
         </div>
     )
 }
-
 export default Home;
